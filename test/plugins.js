072397f1a2c55014729f3ba9cd6e37c774668902
Howard Tyson <howard@howardtyson.com>
2016-02-24 07:12:43 -0800

    Updating test suite to automagically test all plugins.

--- test/plugins.js
+++ test/plugins.js
@@ -3,40 +3,11 @@
 /* global describe it */
 var should = require('should')
 var through2 = require('through2')
+var async = require('async');
 
-var ProboEvents = require('..')
-var Producer = ProboEvents.Producer
+var lib = require('..')
+var plugins = lib.plugins;
 
-var _stream = Symbol('_stream');
-class MemoryProducer {
-  constructor(options, done) {
-    if (typeof options === 'function') {
-      done = options;
-      options = {};
-    }
-    options = options || {};
-    this[_stream] = options.stream || through2();
-    if (done) done();
-  }
-  get stream() {
-    return this[_stream];
-  }
-}
-
-class MemoryConsumer {
-  constructor(options, done) {
-    if (typeof options === 'function') {
-      done = options;
-      options = {};
-    }
-    options = options || {};
-    this[_stream] = options.stream || through2();
-    if (done) done();
-  }
-  get stream() {
-    return this[_stream];
-  }
-}
 
 function historyStream(done) {
   var history = [];
@@ -50,29 +21,48 @@ function historyStream(done) {
   return through2.obj(appender, reporter);
 }
 
-describe('Producer', function() {
-  it('should write an event to the configured handler plugin', function(done) {
-    var stream = through2.obj();
-    var producer = new MemoryProducer({stream});
-    var consumer = new MemoryConsumer({stream});
-    consumer.stream.pipe(historyStream(function(error, events) {
-      should.exist(events);
-      events.length.should.equal(3);
-      events[0].should.equal('data');
-      events[1].foo.should.equal('bar');
-      events[2].should.equal('more data');
-      done(error);
-    }));
-    should.exist(producer.stream);
-    producer.stream.write('data');
-    producer.stream.write({foo: 'bar'});
-    producer.stream.end('more data');
-  })
-})
+/**
+ * Creates a test suite for a given plugin.
+ *
+ * This is necessary in order to close over the plugin in scope.
+ */
+function getSuite(plugin) {
+  return function() {
+    var Producer = plugins[plugin].Producer;
+    var Consumer = plugins[plugin].Consumer;
+    describe(plugin, function() {
+      it('should read message from the consumer that were written to the producer', function(done) {
+        var stream = through2.obj();
+        var instantiator = function(Plugin, cb) {
+          new Plugin({}, cb);
+        };
+        async.parallel({producer: instantiator.bind(null, Producer), consumer: instantiator.bind(null, Consumer)}, function() {
+          var producer = new Producer({stream});
+          var consumer = new Consumer({stream});
+          var cleanup = function(done) {
+            async.parallel([producer.destroy, consumer.destroy], done);
+          };
+          consumer.stream.pipe(historyStream(function(error, events) {
+            if (error) cleanup(done);
+            should.exist(events);
+            events.length.should.equal(3);
+            events[0].should.equal('data');
+            events[1].foo.should.equal('bar');
+            events[2].should.equal('more data');
+            cleanup(done);
+          }));
+          should.exist(producer.stream);
+          producer.stream.write('data');
+          producer.stream.write({foo: 'bar'});
+          producer.stream.end('more data');
+        });
+      })
+    });
+  };
+}
 
-describe('Consumer', function() {
-  it('should receive events from the configured handler plugin', function(done) {
-    var consumerPlugin = new MemoryConsumer()
-    return done()
-  })
+describe('Plugins', function() {
+  for (let plugin in plugins) {
+    getSuite(plugin)();
+  }
 })
