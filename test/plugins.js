14b6e9a701c00175852b4bcfac79e40ec91c9d17
Howard Tyson <howard@howardtyson.com>
2016-02-26 09:19:24 -0800

    Getting the kafka plugin implemented, working properly, and passing a common test suite with the memory plugin.

--- test/plugins.js
+++ test/plugins.js
@@ -1,30 +1,22 @@
-'use strict'
+'use strict';
 
 /* global describe it */
-var should = require('should')
-var through2 = require('through2')
+var should = require('should');
+var through2 = require('through2');
 var async = require('async');
 
-var lib = require('..')
+var lib = require('..');
 var plugins = lib.plugins;
 
-
-function historyStream(done) {
-  var history = [];
-  var appender = function(data, enc, cb) {
-    history.push(data);
-    cb(null, data);
-  };
-  var reporter = function() {
-    if (done) done(null, history);
-  };
-  return through2.obj(appender, reporter);
-}
 
 /**
  * Creates a test suite for a given plugin.
  *
  * This is necessary in order to close over the plugin in scope.
+ *
+ * @param {Object} plugin - A plugin object containing `Producer` and `Consumer`.
+ * @return {Function} - A function with the relevant plugin closed over to allow
+ * properly scoped async iteration.
  */
 function getSuite(plugin) {
   return function() {
@@ -34,35 +26,51 @@ function getSuite(plugin) {
       it('should read message from the consumer that were written to the producer', function(done) {
         var stream = through2.obj();
         var instantiator = function(Plugin, cb) {
-          new Plugin({}, cb);
+          new Plugin({topic: 'test', stream}, cb);
         };
-        async.parallel({producer: instantiator.bind(null, Producer), consumer: instantiator.bind(null, Consumer)}, function() {
-          var producer = new Producer({stream});
-          var consumer = new Consumer({stream});
+        async.parallel({producer: instantiator.bind(null, Producer), consumer: instantiator.bind(null, Consumer)}, function(error, plugins) {
+          var producer = plugins.producer;
+          var consumer = plugins.consumer;
           var cleanup = function(done) {
             async.parallel([producer.destroy, consumer.destroy], done);
           };
-          consumer.stream.pipe(historyStream(function(error, events) {
-            if (error) cleanup(done);
-            should.exist(events);
-            events.length.should.equal(3);
-            events[0].should.equal('data');
-            events[1].foo.should.equal('bar');
-            events[2].should.equal('more data');
-            cleanup(done);
+          var consumedEvents = [];
+          consumer.stream.pipe(through2.obj(function(data, enc, cb) {
+            this.push(data);
+            consumedEvents.push(data);
+            if (consumedEvents.length === 3) {
+              setTimeout(function() {
+                try {
+                  should.exist(consumedEvents);
+                  consumedEvents.length.should.equal(3);
+                  consumedEvents[0].foo.should.equal('bar');
+                  consumedEvents[1].baz.should.equal('bot');
+                  consumedEvents[2].captain.should.equal('spock');
+                  cleanup(done);
+                }
+                catch (e) {
+                  cleanup(done);
+                  throw e;
+                }
+              // Note: this is set such that the autocommit interval will be met.
+              }, 5);
+            }
+            cb();
           }));
           should.exist(producer.stream);
-          producer.stream.write('data');
           producer.stream.write({foo: 'bar'});
-          producer.stream.end('more data');
+          producer.stream.write({baz: 'bot'});
+          producer.stream.end({captain: 'spock'});
         });
-      })
+      });
     });
   };
 }
 
 describe('Plugins', function() {
   for (let plugin in plugins) {
-    getSuite(plugin)();
+    if (plugins.hasOwnProperty(plugin)) {
+      getSuite(plugin)();
+    }
   }
-})
+});
